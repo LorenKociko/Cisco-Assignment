e5b2d1c1efce9fb5d52434540135102ef439a070
Loren Kociko <std137547@ac.eap.gr>
2022-04-20 00:50:10 +0300

    Custom Validators on register, and storing user in db

--- Test/db.py
+++ Test/db.py
@@ -1,19 +1,23 @@
 from encodings import utf_8
 import pymongo
 from pymongo import MongoClient
+import re
 
 
-with open("password.txt","r") as f:
-    psw = f.readline()
+# with open("password.txt","r") as f:
+#     psw = f.readline()
 
-connection_sting = f"mongodb+srv://cisco:{psw}@cisco.wzrhr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+connection_sting = f"mongodb+srv://cisco:C1sco12345@cisco.wzrhr.mongodb.net/Cisco_Assigment?retryWrites=true&w=majority"
 cluster = MongoClient(connection_sting)
 
 db = cluster["Cisco_Assigment"]
 user_collection = db["user"]
 
-user = {"name":"test","score":5}
+user = {"name":"TesT","score":5}
 user_collection.insert_one(user)
 
 r = user_collection.find_one({"name":"test"})
-print(r)
+username = "TesT"
+user = user_collection.find_one({"name": re.compile('^' + username + '$', re.IGNORECASE)})
+print(user)
+
